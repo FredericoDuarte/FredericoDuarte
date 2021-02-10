08057b901958a2dfb56c7f05fb49a8f5db41e455
FredericoDuarte <frederico.duarte@brwo.com.br>
2021-02-10 16:34:51 -0300

    Alterações no codigo do Alura Typer

--- Curso JQuery Alura/alura-typer/public/js/main.js
+++ Curso JQuery Alura/alura-typer/public/js/main.js
@@ -5,6 +5,17 @@ var numPalavras = frase.split(" ").length;
 //encontra o conteudo que eu quero pela id e . text retorna o valor da quantidade
 var tamanhoDaFrase = $("#tamanho-frase").text(numPalavras);
 
+// Selecionando o campo de digitacao pela sua id HTML
+var campo = $(".campo-digitacao");
+//Função para escutar o evento no caso o clique do mouse
+campo.on("input", function(){
+    var conteudo = campo.val();
 
+    var qtdPalavras = conteudo.split(/\S+/).length - 1; // (/\S+/) significa para procurar todos os espaços vazios
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qrdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qrdCaracteres);
+});
 
 
